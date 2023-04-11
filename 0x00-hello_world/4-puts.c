9d5a3496af475131471d6a73e41de9378d2320f4
kababa <pogutu21@gmail.com>
2023-04-11 07:59:16 -0700

    4-puts.c corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * Return: 0 if success
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+	{
+Char str [50]="\"Programming is like building a multilingual puzzle"; 		
+puts(str);
 return (0);
 	}
